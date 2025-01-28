ba903ee96a65b684efd671dae698a5d671b5f34d
Juan Sebastian Vinasco Salinas <js.vinasco.s@gmail.com>
2025-01-28 17:38:26 -0500

    clean code

--- cgiar_root_volume_estimation/dataset.py
+++ cgiar_root_volume_estimation/dataset.py
@@ -4,26 +4,26 @@ import typer
 from loguru import logger
 from tqdm import tqdm
 
-from cgiar_root_volume_estimation.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
+# from cgiar_root_volume_estimation.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
 
-app = typer.Typer()
-
-
-@app.command()
-def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = RAW_DATA_DIR / "dataset.csv",
-    output_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
-    # ----------------------------------------------
-):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Processing dataset...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Processing dataset complete.")
-    # -----------------------------------------
+# app = typer.Typer()
 
 
-if __name__ == "__main__":
-    app()
+# @app.command()
+# def main(
+#     # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
+#     input_path: Path = RAW_DATA_DIR / "dataset.csv",
+#     output_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
+#     # ----------------------------------------------
+# ):
+#     # ---- REPLACE THIS WITH YOUR OWN CODE ----
+#     logger.info("Processing dataset...")
+#     for i in tqdm(range(10), total=10):
+#         if i == 5:
+#             logger.info("Something happened for iteration 5.")
+#     logger.success("Processing dataset complete.")
+#     # -----------------------------------------
+
+
+# if __name__ == "__main__":
+#     app()
